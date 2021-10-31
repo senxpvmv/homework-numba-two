a743f37741e4d8abf50c2b73683d77563daf07bf
senxpvmv <max_martin@ro.ru>
2021-11-01 00:52:27 +0200

    a

---
+++ 1.js
@@ -0,0 +1,70 @@
+// let first = +prompt("Введіть перше число")
+// let second = +prompt("Введіть друге число")
+// let sum = 0
+//
+// for(i=first; i<=second; i++) {
+//     sum += i;
+// }
+
+
+// let arr = ["a", "b", "c", "d", "x"];
+//
+// while (true) {
+//     let operator = prompt("Що ми будемо робити? \n Ми можемо: видалити (shift | pop) чи додати (unshift | push) або вивести один із елементів (1 | 2 | 3 | 4 | 5)")
+//
+//     if (operator === "shift") {
+//         arr.shift();
+//         alert(arr);
+//     } else if (operator === "pop") {
+//         arr.pop();
+//         alert (arr);
+//     } else if (operator === "unshift") {
+//         let value = prompt("Введіть значення")
+//         arr.unshift(value);
+//         alert (arr);
+//     } else if (operator === "push") {
+//         let value = prompt("Введіть значення")
+//         arr.push(value);
+//         alert(arr);
+//     } else if (operator === "1") {
+//         alert(arr[0]);
+//     } else if (operator === "2") {
+//         alert(arr[1]);
+//     } else if (operator === "3") {
+//         alert(arr[2]);
+//     } else if (operator === "4") {
+//         alert(arr[3]);
+//     } else if (operator === "5") {
+//         alert(arr[4]);
+//     }
+// }
+
+
+
+// let length = +prompt("Введіть довжину");
+// let width = +prompt("Введіть ширину");
+// let square = 0;
+//
+// function ar () {
+//     square = length**2
+// };
+//
+// function are () {
+//     square = width**2
+// };
+//
+// function area () {
+//     square = length*width
+// };
+//
+// if (length !== 0 && width !== 0) {
+//     area ()
+//     alert(square)
+// } else if (length <= 0 || length === " " ) {
+//     are ()
+//     alert (square)
+// } else if (width <= 0 || width === " " ) {
+//     ar ()
+//     alert (square)
+// }
+
